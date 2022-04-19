b4587b795185d2431d833c2bbd93fd7e7545d5de
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2022-04-19 22:54:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,107 +2,107 @@ const frame = [
     {
         category : 'breakfast',
         image: "20210914_172354.jpg",
-        title : 'Bread and Beans',
+        title : 'Bread',
         price : '50',
-        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quia architecto ipsa quis provident iste quibusdam consequuntur in quae Lorem ipsum dolor sit, amet consectetur adipisicing elit..' 
+        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quia architecto ipsa quis provident iste quibusdam consequuntur in quae Lorem ipsum dolor sit.' 
     },
     {
         category : 'breakfast',
         image: "20210918_200420.jpg",
-        title : 'Bread and Beans',
+        title : 'Beans',
         price : '53',
-        details: ' Commodi labore repellat excepturi earum sequi molestias repudiandae non odio ea quisquam ab voluptates dicta debitis quo, tempora totam nam magni similique consequatur est at placeat facilis' 
+        details: ' Commodi labore repellat excepturi earum sequi molestias repudiandae non odio ea quisquam ab voluptates dicta debitis quo, tempora totam nam magni.' 
     },
     {
         category : 'lunch',
         image: "20211116_120211.jpg",
-        title : 'Bread and Beans',
+        title : 'Amala',
         price : '65',
-        details: 'Autem assumenda eaque sit voluptatem consectetur dolorum et. Ducimus quam quos vero ipsum illum dicta in veniam quod voluptate recusandae aut assumenda voluptatibus quo, libero, delectus facere illo rerum labore! Tempora voluptatum, distinctio accusamus corrupti autem perspiciatis nisi fugiat maiores' 
+        details: 'Autem assumenda eaque sit voluptatem consectetur dolorum et. Ducimus quam quos vero ipsum illum dicta in veniam quod voluptate recusandae aut assumenda voluptatibus.' 
     },
     {
         category : 'shakes',
         image: "20211126_234427.jpg",
-        title : 'Bread and Beans',
+        title : 'yam',
         price : '48',
-        details: 'quo repudiandae. Inventore, neque tenetur sit nesciunt consequuntur, optio illum fuga modi eligendi non corporis dolor explicabo animi. Dolorum unde alias molestiae quod, ad labore aspernatur nam harum, voluptas minus explicabo? Reiciendis corrupti quia quam voluptatibus dolore possimus eligendi' 
+        details: 'quo repudiandae. Inventore, neque tenetur sit nesciunt consequuntur, optio illum fuga modi eligendi non corporis dolor explicabo animi. Dolorum unde alias molestiae quod.' 
     },
     {
         category : 'dinner',
         image: "20220207_195528.jpg",
-        title : 'Bread and Beans',
+        title : 'susage roll',
         price : '83',
-        details: 'aperiam vel. Nam, dolorem culpa? Sequi est libero nemo blanditiis dolores aliquid optio! Ut delectus officiis, laboriosam et accusantium ipsam neque facere est tempore adipisci sapiente, nesciunt molestiae vel libero! Cupiditate vitae ut dolores eaque repudiandae debitis, eius culpa temporibus dolorem modi nam ipsam necessitatibus!' 
+        details: 'aperiam vel. Nam, dolorem culpa? Sequi est libero nemo blanditiis dolores aliquid optio! Ut delectus officiis, laboriosam et accusantium ipsam neque facere est tempore!' 
     },
     {
         category : 'shakes',
-        image: "20210918_200420.jpg",
-        title : 'Bread and Beans',
+        image: "20210914_172354.jpg",
+        title : 'sandwich',
         price : '36',
-        details: 'praesentium vel minima dolores alias. Ab repellendus neque a illum, eos nostrum doloribus et tenetur. Fugit obcaecati suscipit voluptas animi possimus illum dolore expedita explicabo temporibus non autem magnam iure maiores quis libero vitae, dicta nihil officia quisquam perspiciatis molestiae,' 
+        details: 'praesentium vel minima dolores alias. Ab repellendus neque a illum, eos nostrum doloribus et tenetur. Fugit obcaecati suscipit voluptas animi possimus illum dolore expedita.' 
     },
     {
         category : 'lunch',
-        image: "20220207_195528.jpg",
-        title : 'Bread and Beans',
+        image: "20210918_200420.jpg",
+        title : 'chicken',
         price : '29',
-        details: 'rem nihil repellendus, doloremque molestias dolores doloribus vel fuga voluptatibus quisquam dolore cupiditate. Fuga consequatur magni quod accusamus eaque, vel natus atque nisi. Harum qui, corrupti eaque doloribus quis tempore adipisci voluptates dolorum asperiores mollitia est, nulla debitis officia' 
+        details: 'rem nihil repellendus, doloremque molestias dolores doloribus vel fuga voluptatibus quisquam dolore cupiditate. Fuga consequatur magni quod accusamus eaque, vel natus.' 
     },
     {
         category : 'shakes',
-        image: "20210914_172354.jpg",
-        title : 'Bread and Beans',
+        image: "20210918_200420.jpg",
+        title : 'parfait',
         price : '14',
-        details: 'voluptate obcaecati sint recusandae quisquam totam enim! Ea, asperiores! Blanditiis incidunt fugiat doloribus atque mollitia voluptatibus voluptas suscipit amet, earum natus commodi ex doloremque architecto temporibus alias! Earum omnis reiciendis adipisci, in molestias cumque' 
-    },
-    {
-        category : 'dinner',
-        image: "20210918_200420.jpg",
-        title : 'Bread and Beans',
-        price : '67',
-        details: 'doloremque nam. Iusto ducimus voluptatibus, officia reiciendis possimus unde doloremque error, quod magnam labore ut exercitationem vitae rerum vel nihil optio voluptas mollitia! Atque eum ducimus possimus, dicta fugiat cupiditate? Aliquid maiores dolorum atque rerum excepturi amet ad, harum,' 
-    },
-    {
-        category : 'lunch',
-        image: "20220207_195528.jpg",
-        title : 'Bread and Beans',
-        price : '37',
-        details: 'animi id accusantium temporibus nam esse nemo nobis aliquam ex molestiae eum velit distinctio facere maxime perspiciatis! Beatae vero a voluptatum assumenda accusantium ducimus aliquid nam omnis maiores adipisci quaerat et, repudiandae dolorum recusandae voluptatibus eaque veniam saepe, quia commodi' 
+        details: 'voluptate obcaecati sint recusandae quisquam totam enim! Ea, asperiores! Blanditiis incidunt fugiat doloribus atque mollitia voluptatibus voluptas suscipit amet.' 
     },
     {
         category : 'dinner',
         image: "20210914_172354.jpg",
-        title : 'Bread and Beans',
-        price : '61',
-        details: 'Beatae, repellat hic. Ipsa quisquam nostrum aperiam, veritatis assumenda debitis voluptate commodi incidunt eligendi consectetur nesciunt molestias adipisci unde repellendus at velit sint ducimus odit, quo tenetur aliquid laborum minus tempora! Quae at iste odit, recusandae id cupiditate' 
+        title : 'smoothie',
+        price : '67',
+        details: 'doloremque nam. Iusto ducimus voluptatibus, officia reiciendis possimus unde doloremque error, quod magnam labore ut exercitationem vitae rerum vel nihil optio voluptas mollitia!.' 
     },
     {
         category : 'lunch',
+        image: "20211126_234427.jpg",
+        title : 'fruit',
+        price : '37',
+        details: 'animi id accusantium temporibus nam esse nemo nobis aliquam ex molestiae eum velit distinctio facere maxime perspiciatis! Beatae vero a voluptatum assumenda accusantium ducimus.' 
+    },
+    {
+        category : 'dinner',
         image: "20210918_200420.jpg",
-        title : 'Bread and Beans',
+        title : 'juice',
+        price : '61',
+        details: 'Beatae, repellat hic. Ipsa quisquam nostrum aperiam, veritatis assumenda debitis voluptate commodi incidunt eligendi consectetur nesciunt molestias adipisci under.' 
+    },
+    {
+        category : 'lunch',
+        image: "20211126_234427.jpg",
+        title : 'coffee',
         price : '43',
-        details: ' Distinctio, qui ipsam. Dolore, ad ducimus aut eligendi doloribus ipsum consequuntur quam harum consectetur odit vitae eius dignissimos, vero, aliquid est.  obcaecati ad quibusdam ratione cum quod, error delectus culpa iusto fuga perspiciatis voluptatem beatae pariatur reprehenderit corporis dicta' 
+        details: ' Distinctio, qui ipsam. Dolore, ad ducimus aut eligendi doloribus ipsum consequuntur quam harum consectetur odit vitae eius dignissimos, vero, aliquid est.  obcaecati ad quibusdam.' 
     },
     {
         category : 'breakfast',
-        image: "20210914_172354.jpg",
-        title : 'Bread and Beans',
+        image: "20211116_120211.jpg",
+        title : 'vegetables',
         price : '73',
-        details: 'Repellat illo optio doloribus, sapiente animi quos possimus aspernatur reprehenderit autem magnam dolore, nostrum commodi odit nulla beatae quaerat id molestiae, omnis quam accusantium quo? Officia iusto repellendus aliquid et a, repudiandae saepe aut unde officiis at laboriosam, veniam' 
+        details: 'Repellat illo optio doloribus, sapiente animi quos possimus aspernatur reprehenderit autem magnam dolore, nostrum commodi odit nulla beatae quaerat id molestiae, omnis quam .' 
     },
     {
         category : 'dinner',
         image: "20211116_120211.jpg",
-        title : 'Bread and Beans',
+        title : 'sharwarma',
         price : '32',
-        details: 'Explicabo odio quaerat labore deleniti consequuntur facilis commodi nemo minima at, deserunt iure soluta harum repellat adipisci, vitae voluptatibus eum veritatis autem vel nostrum hic quia dolore eligendi perferendis. quasi?  dolorem asperiores nihil maiores. Ab, eius' 
+        details: 'Explicabo odio quaerat labore deleniti consequuntur facilis commodi nemo minima at, deserunt iure soluta harum repellat adipisci, vitae voluptatibus eum .' 
     },
     {
         category : 'breakfast',
-        image: "20210914_172354.jpg",
-        title : 'Bread and Beans',
+        image: "20220207_195528.jpg",
+        title : 'cold stone',
         price : '63',
-        details: 'Delectus repellat adipisci eum autem incidunt quam distinctio consectetur laboriosam neque minus, unde explicabo nihil!  id dignissimos sit voluptates praesentium, magni asperiores error iure facilis rerum, reiciendis iusto! Culpa dolore eos ex omnis voluptatem reprehenderit.' 
+        details: 'Delectus repellat adipisci eum autem incidunt quam distinctio consectetur laboriosam neque minus, unde explicabo nihil!  id dignissimos sit voluptates praesentium.' 
     }
 ];
 
@@ -147,8 +147,8 @@ function displayFrameItem(frameItem) {
             <img src=${item.image} alt=${item.title}>
         <div class="info">
             <div class="title_price">
-                <p >${item.title}</p>
-                <p class="price">$${item.price}</p>
+                <p > <strong> ${item.title}</strong>  </p>
+                <p class="price"><strong>$${item.price}</strong></p>
             </div>
             <p class="details">${item.details}</p>
             </div>
