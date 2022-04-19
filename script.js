30e9fabe82d6d64f71fa39dfc3b15041cda7b136
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2022-04-19 08:35:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ buttons.forEach(function(btn){
 
 
 function displayFrameItem(frameItem) {
-    let displayFrame = frame.map(function(item){
+    let displayFrame = frameItem.map(function(item){
     
         return   `<div class="container">
             <img src=${item.image} alt=${item.title}>
