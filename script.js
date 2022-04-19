43edbaba05a4e4b3d5dc4ecfd0eb7af405922d66
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2022-04-19 10:28:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ const buttons = document.querySelectorAll('.btns');
 
 
 //load items
-window.addEventListener('DOMContentLoaded',function()
+window.addEventListener('DOMContentLoaded',function(frame)
 {
    displayFrameItem(frame)
 });
